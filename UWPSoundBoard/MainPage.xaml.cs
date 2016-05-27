3ed9bf7fe2e270297f3b8014b5ae7619aa65a5c2
Giovan <Giovan@CARBONVECTOR02>
2016-05-27 17:33:31 +0700

    Drag and Drop

--- UWPSoundBoard/MainPage.xaml.cs
+++ UWPSoundBoard/MainPage.xaml.cs
@@ -5,8 +5,10 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UWPSoundBoard.Model;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -56,7 +58,11 @@ namespace UWPSoundBoard
 
         private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
-
+            // removing the temporary variables
+            /* SearchAutoSuggestBox.ItemsSource = Sounds
+                .Where(p => p.Name.StartsWith(((AutoSuggestBox)sender).Text))
+                .ToList();
+            */
         }
 
         private void SearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
@@ -76,6 +82,39 @@ namespace UWPSoundBoard
         {
             // var sound = (Sound)e.ClickedItem; skipped this part :v
             MyMediaElement.Source = new Uri(this.BaseUri, ((Sound)e.ClickedItem).AudioFile);
+        }
+
+        private async void SoundGridView_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+
+                if (items.Any())
+                {
+                    var storageFile = items[0] as StorageFile;
+                    var contentType = storageFile.ContentType;
+
+                    StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+                    if (contentType == "audio/wav" || contentType == "audio/mpeg")
+                    {
+                        StorageFile newFile = await 
+                            storageFile.CopyAsync(folder, storageFile.Name, NameCollisionOption.GenerateUniqueName);
+                        MyMediaElement.SetSource(await storageFile.OpenAsync(FileAccessMode.Read), contentType);
+                        MyMediaElement.Play();
+                    }
+                }
+            }
+        }
+
+        private void SoundGridView_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "drop to create a custom sound and tile";
+            e.DragUIOverride.IsCaptionVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            e.DragUIOverride.IsGlyphVisible = true;
         }
     }
 }
